67e4fadb6aeed1a7a525e9e7d36ccff4847de875
javiow <javiow@naver.com>
2021-11-16 23:59:30 +0900

    add unittest CI

---
+++ main_test.py
@@ -0,0 +1,11 @@
+import unittest
+import main
+
+
+class MainTest(unittest.TestCase):
+    def test_helloworld(self):
+        ret = main.helloworld("Test")
+        self.assertEqual(ret, "Hello World! Chris!")
+
+if __name__ == "__main__":
+    unittest.main()
